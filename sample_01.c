7fa13247dcbc9b7577efc103e2b04bab041ed06f
HumanITNagoya <51362998+HumanITNagoya@users.noreply.github.com>
2019-06-04 19:09:19 +0900

    Update sample_01.c

--- sample_01.c
+++ sample_01.c
@@ -13,7 +13,7 @@ int main()
     /* 文字の出力 */
     printf( "2文字目は%c\n" , *(pt+1) );
 
-    pt = ex + 4 ;   /* ポインタの場所を変更 */
+    pt = ex + 4 ;   /* アドレスを変更 */
     printf( "5文字目は%c\n" , *pt );
 
     return 0;
